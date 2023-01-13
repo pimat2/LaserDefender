1e0beb568b4de22d8ef3d7cefc401412622054f0
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-14 00:32:26 +0200

    Created a Scriptable Object
    This Scriptable Object will contain the information about which enemies will be spawned in the current wave, the path that the enemies have to follow(using a list of waypoints), the time between enemy spawns and what the enemy movement speed will be

---
+++ LaserDefender/Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5f;
+    public Transform GetStartingWaypoin(){
+        return pathPrefab.GetChild(0);
+    }
+    public List<Transform> GetWaypoints(){
+        List<Transform> Waypoints = new List<Transform>();
+        foreach(Transform child in pathPrefab)
+        {
+            Waypoints.Add(child);
+        }
+        return Waypoints;
+    }
+    public float GetMoveSpeed(){
+        return moveSpeed;
+    }
+
+}
