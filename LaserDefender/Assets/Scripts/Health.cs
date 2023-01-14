c2c2d9371dc5f5a993d6124f3d6fc9c867ec2b89
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-14 03:14:29 +0200

    Particle System
    Implementation of a Particle System when the player shoots an enemy or gets shot by an enemy

--- LaserDefender/Assets/Scripts/Health.cs
+++ LaserDefender/Assets/Scripts/Health.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+
    [SerializeField] int health = 50;
+   [SerializeField] ParticleSystem hitEffect;
    void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
         if(damageDealer != null){
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
             damageDealer.Hit();
         }
    }
@@ -17,5 +20,12 @@ public class Health : MonoBehaviour
      if(health <= 0){
         Destroy(gameObject);
      }
+   }
+   void PlayHitEffect(){
+    if(hitEffect != null){
+        
+        ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+    }
    }
 }
